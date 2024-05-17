cee5a410cfab3485c7e29d50898cf3357a59926b
Rasinhas <felipe.rasinhas@hashdex.com>
2024-05-17 16:36:56 -0300

    fix

--- mod2/main.tf
+++ mod2/main.tf
@@ -1,5 +1,5 @@
 module "cloudwatch" {
-  source   = "git@github.com:hashdex/hdx-vulcan.git//aws/cloudwatch/log_group?ref=main"
+  source   = "git@github.com:hdx-rasinhas/test-module.git//mod1?ref=main"
   app_name = var.app_name
   env      = var.env
 }
